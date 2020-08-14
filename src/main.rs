aac8fe71843ccb078d6637f69e9d7ed1323d1524
Andres O. Vela <andresovela@gmail.com>
2020-08-14 23:36:55 +0200

    Added `probe-run` support, `cargo embed/flash` support, and RTT printing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
-use cortex_m::asm;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
 use cortex_m_rt::entry;
+
+use stm32f1xx_hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
+    rtt_init_print!(BlockIfFull, 16384);
+    rprintln!("Hello, world!");
     loop {
-        // your code goes here
+        continue;
     }
 }
